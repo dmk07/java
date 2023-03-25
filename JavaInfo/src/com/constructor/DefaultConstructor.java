16ce4c728892561e8b6540105bea005f699293e0
dmk61 <dmk61@Dipak-Kadam>
2023-03-25 17:15:33 +0530

    1.0

---
+++ JavaInfo/src/com/constructor/DefaultConstructor.java
@@ -0,0 +1,27 @@
+package com.constructor;
+
+public class DefaultConstructor {
+	
+	DefaultConstructor()
+	{
+		System.out.print("Default Constructor");
+	}
+	
+	// private constructor
+	
+	private DefaultConstructor(int a)
+	{
+		System.out.print("private constructor"+a);
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+	
+		new DefaultConstructor(5);
+		
+
+	}
+
+}
