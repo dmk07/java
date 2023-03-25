0a2a0fc6fe4a7101ec7b5fba3f17dee4c3c23ed3
dmk61 <dmk61@Dipak-Kadam>
2023-03-25 17:53:14 +0530

    1.7

--- JavaInfo/src/com/arrays/ArrayDeclareation.java
+++ JavaInfo/src/com/arrays/ArrayDeclareation.java
@@ -3,7 +3,7 @@ package com.arrays;
 public class ArrayDeclareation {
 
 	public static void main(String[] args) {
-	
+	// java array
 		int[] array= {10,22,2,43,43,55,44,31,2,1,4,5,3,525,35};
 		
 		 System.out.print(array); // printing array hashcode
