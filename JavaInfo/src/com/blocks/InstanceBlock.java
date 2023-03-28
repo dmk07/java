55383028207639550ca3902077472a425ea3bed7
dmk61 <dmk61@Dipak-Kadam>
2023-03-28 19:19:25 +0530

    1.0

--- JavaInfo/src/com/blocks/InstanceBlock.java
+++ JavaInfo/src/com/blocks/InstanceBlock.java
@@ -5,7 +5,7 @@ public class InstanceBlock {
 	{
 		// instance initilizer block. you can call only if an object of class created.
 		System.out.print("hello Dipak");
-	}
+	}//
 	static {
 		// static block don't need to create obj of class. when ever class is loaded in jvm it automaticlly call static block
 		System.out.print(12+4334);
